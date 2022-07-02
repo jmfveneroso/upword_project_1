78e78be4ed571b32e6de4224e8c3d86c132d58c2
iaralucia <iaralucia22@gmail.com>
2022-07-02 11:20:10 -0300

    CSS changes requested by client.

--- senior_panel/static/senior_panel.js
+++ senior_panel/static/senior_panel.js
@@ -8,18 +8,12 @@ function showTime(){
   var h = date.getHours();
   var m = date.getMinutes();
   var s = date.getSeconds();
-  var session = "AM"
-
-  if(h>12){
-      h = h - 12;
-      session = "PM"
-  }
 
   h = (h<10) ? "0" + h : h;
   m = (m<10) ? "0" + m : m;
   s = (s<10) ? "0" + s : s; 
   
-  var time = h + ":" + m + ":" + s + " " + session;
+  var time = h + ":" + m + ":" + s;
   document.getElementById("clock").innerHTML = time;
   document.getElementById("clock").textContent = time;
 
